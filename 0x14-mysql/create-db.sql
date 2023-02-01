eb445bf8512ca49dd2b00943a2330ed90d34ae9b
arsemakidist <kidist.shiferaw@aau.edu.et>
2023-02-01 08:03:11 -0800

    task 14sql

--- 0x14-mysql/create-db.sql
+++ 0x14-mysql/create-db.sql
@@ -4,13 +4,10 @@ CREATE DATABASE IF NOT EXISTS tyrell_corp;
 -- Create table
 CREATE TABLE IF NOT EXISTS tyrell_corp.nexus6
 (
-		id INT UNIQUE AUTO_INCREMENT NOT NULL,
-		name VARCHAR(256) NOT NULL,
-		PRIMARY KEY (id),
-
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
 );
 
 -- Insert an entry
 INSERT INTO `tyrell_corp.nexus6` (`name`) VALUES ("Emmastro");
-
-
